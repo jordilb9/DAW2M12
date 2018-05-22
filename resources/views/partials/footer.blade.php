257bde84f89eb747222679f59b3bf1003e40fef1
jordi <jordi@DESKTOP-NG1IKRR>
2018-05-22 17:49:15 +0200

    principal

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -9,7 +9,7 @@
                     <div class="footer-pad">
                         <h4>Acceso rápido</h4>
                         <ul class="list-unstyled">
-                            <li><a href="#"></a></li>
+                            <li><a href="{{url('/')}}">Menú Principal</a></li>
                             <li><a href="#">Blog</a></li>
                             <li><a href="#">Blog</a></li>
                             <li><a href="#">Liga</a></li>
@@ -23,12 +23,12 @@
                     <!--Column1-->
                     <div class="footer-pad">
                         <h4>Popular Departments</h4>
-                        <ul class="list-unstyled">
-                            <li><a href="#" class="fa fa-twitter"></a></li>
+                        <ul class="list-unstyled ">
+                            <li><a href="https://twitter.com/lepaladins?lang=es" class="fa fa-twitter"></a> Siguenos</li>
                             <br>
-                            <li><a href="#" class="fa fa-youtube"></a></li>
+                            <li><a href="https://www.youtube.com/channel/UCiwC8PyMEef0j9xm3YW81CQ" class="fa fa-youtube"></a> Suscribete</li>
                             <br>
-                            <li><a href="#" class="fa fa-twitch"></a></li>
+                            <li><a href="https://www.twitch.tv/ligadepaladinsesp" class="fa fa-twitch"></a> Suscribete</li>
                             <li>
                                 <a href="#"></a>
                             </li>
@@ -49,3 +49,4 @@
         </div>
     </div>
 </footer>
+
