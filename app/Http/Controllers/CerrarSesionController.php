d53fb473b2aa68b8539418c4672bfea424e21808
jordi <jordi@DESKTOP-NG1IKRR>
2018-05-22 17:42:51 +0200

    principal

--- app/Http/Controllers/CerrarSesionController.php
+++ app/Http/Controllers/CerrarSesionController.php
@@ -6,24 +6,24 @@ use Illuminate\Http\Request;
 use Session;
 use App\Logs;
 
-class CerrarSesionController extends Controller
-{
-    public function cerrarSesion()
-    {
+class CerrarSesionController extends Controller {
+
+    public function cerrarSesion() {
         //Iniciem sessió
         session_start();
-        
+
         //Creem una nova variable de tipus logs i guardem l'idUsuari, descripcio, dataLog, hora i path
-         $log = new Logs;
-           $log->IdUsuario=$_SESSION['idUsuario'];
-           $log->Descripcion="El usuario ".$_SESSION['User']." a cerrado sesion";
-           $log->Fechaq =date('Y-m-d H:i:s');
-           $log->save();
-        
+        $log = new Logs;
+        $log->IdUsuario = $_SESSION['idUsuario'];
+        $log->Descripcion = "El usuario " . $_SESSION['User'] . " a cerrado sesion";
+        $log->Fechaq = date('Y-m-d H:i:s');
+        $log->save();
+
         //Utilitzem el metode flush() per borrar tota la informació de la sessió
         //Session::flush();
-         session_destroy();
+        session_destroy();
         //Redirigim al CU01
         return redirect('/');
     }
+
 }
